863e289fa215d57635f1412a8e2a5a07d77f568b
Atsu3Sh1n1 <waborinohouou@gmail.com>
2025-05-24 16:43:22 +0900

    更新内容

--- project-top/project-root/fireworks.js
+++ project-top/project-root/fireworks.js
@@ -163,7 +163,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // 3秒後にアニメーションを開始
   setTimeout(() => {
     animate();
-  }, 3000); // 3000ミリ秒 (3秒)
+  }, 1600); // 3000ミリ秒 (3秒)
 
   // リサイズイベントの最適化
   let resizeTimeout;
